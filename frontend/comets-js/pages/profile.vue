e92090d25621ccfafef483a561fa0b9d49dd57cf
Changbra <56209371+Changbra@users.noreply.github.com>
2022-03-01 21:10:40 -0800

    Update profile.vue
    Updated user profile

--- frontend/comets-js/pages/profile.vue
+++ frontend/comets-js/pages/profile.vue
@@ -1,18 +1,20 @@
 <template>
    <section class="section">
-      <div class="container">
-         <h2 class="title">My Profile</h2>
-         <div class="content">
-            <p>
-               <strong>Username:</strong>
-               {{ loggedInUser.username }}
-            </p>
-            <p>
-               <strong>Email:</strong>
-               {{ loggedInUser.email }}
-            </p>
-         </div>
-      </div>
+      <div class="p-3">
+        <div class="text-3xl text-gray-700 text-center font-bold">My Profile</div>
+        <div class="text-xl text-gray-700 font-bold text-left ">Account Information</div>
+          <div class="p-1 text-gray-500 font-normal">Username: {{ loggedInUser.username }}</div>
+          <div class="p-1 text-gray-500 font-normal">Email: {{ loggedInUser.email }}</div>
+          <div class="p-1 text-gray-500 font-normal">Shipping Address:</div>
+          <div class="p-1 text-xl text-center text-gray-700 font-bold">Order History</div>
+            <div class="p-1">
+              <ol>
+                <li>1.</li>
+                <li></li>
+                <li></li>
+              </ol>
+            </div>
+      </div>      
    </section>
 </template>
 <script>
